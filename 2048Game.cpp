4058782b28ed5e6ad9e07418553fc091a20659ac
Andrew Mei <andrewmei915@gmail.com>
2019-10-19 15:38:15 -0400

    Added draw function, fixed some bugs

--- 2048Game.cpp
+++ 2048Game.cpp
@@ -5,13 +5,10 @@
 
 using std::cout;
 
-int gameOver;
-bool keyPressed = false;
+int gameOver = 0;
 
-//head pos, rand spawning nums pos, score
-int x, y, numX, numY, score;
-
-// enum eDir {LEFT = 0, RIGHT, UP, DOWN};
+//rand spawning nums pos, grid numbers, score
+int numX, numY, num, score;
 
 //Grid
 const int gridSize = 4;
@@ -19,13 +16,14 @@ int grid[gridSize][gridSize];
 
 void setup() {
     gameOver = 0;
-    x, y = gridSize / 2;
 
     srand((unsigned)time(0)); 
 
-    numX = rand() % 4 + 1; //range of 1-4
-    numY = rand() % 4 + 1;
 
+    numX = rand() % 4; //range of 0-3
+    numY = rand() % 4;
+
+    //prints grid
     for (int column = 0; column < gridSize; column++) {
         for (int row = 0; row < gridSize; row++) {
             grid[column][row] = 0;
@@ -34,29 +32,50 @@ void setup() {
         cout << "\n";
     }
 
+
 }
 
 void draw(){
+    //random generating nums (///////////////////////////////////////////////
+                //change to make num change only when grid changes//
+    ////////////////////////////////////////////////////////////////////////)
+    int i = rand() % 3;
+    if (i == 0) {
+        num = 2;
+    } else {
+        num = 4;
+    }
 
-    
+    //changes grid
+    grid[numY][numX] = num;
+    system("cls");
+    for (int column = 0; column < gridSize; column++) {
+        for (int row = 0; row < gridSize; row++) {
+            cout << grid[column][row] << " ";
+        }
+        cout << "\n";
+    }
 
 }
 
 void up() {
-    cout << "W pressed \n";
-    keyPressed = false;
+    draw();
+    cout << "Up";
 }
 
 void left() {
-    cout << "A pressed \n";
+    draw();
+    cout << "Left";
 }
 
 void down() {
-    cout << "S pressed \n";
+    draw();
+    cout << "Down";
 }
 
 void right() {
-    cout << "D pressed \n";
+    draw();
+    cout << "Right";
 }
 
 void logic() {
@@ -66,7 +85,7 @@ void logic() {
         for (int row = 1; row < gridSize-1; row++) {
             if (grid[row - 1][column - 1] == grid[row][column - 1] || grid[row + 1][column - 1] == grid[row][column - 1]) {
                 break;
-            } else {
+            } else if (column && row == 2) {
                 gameOver++;
             }
         }
@@ -75,27 +94,29 @@ void logic() {
         for (int row = 1; row < gridSize; row++) {
             if (grid[row - 1][column] == grid[row - 1][column + 1] || column - 1 == column) {
                 break;
-            }/* else if (column == 0) {
-                if (column - 1 == column) {
-                    break;
-                }
-            } else if (column == gridSize-1) {
-                if (column + 1 == column) {
-                    break;
-                }
-            }*/ else {
+            } else if (column == 2 && row == 3) {
                 gameOver++;
             }
         }
     }
+    int i;
+    for (int column = 0; column < gridSize; column++) {
+        for (int row = 0; row < gridSize; row++) {
+            if (grid[column][row] != 0) {
+                i++;
+                if (i == 16) {
+                    gameOver++;
+                }
+            }
+        }
+    }
+    if (gameOver > 3) {
+        gameOver = 0;
+    }
 
     //keyboard inputs
     if(GetKeyState('W') & 0x8000/*Check if high-order bit is set (1 << 15)*/) {
-        if (!keyPressed) {
-            up();
-        } else {
-            keyPressed = false;
-        }
+        up();
     }
 
     if(GetKeyState('A') & 0x8000/*Check if high-order bit is set (1 << 15)*/) {
@@ -118,7 +139,7 @@ int main() {
     setup();
     draw();
 
-    while (gameOver < 1) {
+    while (gameOver < 4) {
         logic();
     }
     cout << "Game Over";
